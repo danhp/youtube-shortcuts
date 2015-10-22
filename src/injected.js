d93fca6b88bfeb432c2affad4e46eddb09352cfc
Daniel Pham <pham.dany@gmail.com>
2015-10-22 12:36:12 -0400

    Refactor injected.js

--- src/injected.js
+++ src/injected.js
@@ -1,5 +1,8 @@
 document.addEventListener("keydown", checkShortcuts);
 
+var options = ['focus', 'subbox', 'user', 'like', 'dislike', 'subscribe', 'playlist', 'info',
+               'listDown', 'listUp', 'tabLeft', 'tabRight'];
+
 function checkShortcuts(event) {
     var focus = document.activeElement;
     if (focus.nodeName == "INPUT" || focus.nodeName == "TEXTAREA") {
@@ -12,139 +15,104 @@ function checkShortcuts(event) {
             return;
         }
 
-        if (event.keyCode == o.subbox &&
-                    event.metaKey == o.metasubbox &&
-                    event.ctrlKey == o.ctrlsubbox &&
-                    event.altKey == o.altsubbox &&
-                    event.shiftKey == o.shiftsubbox) {
-            goToSubs();
-            return;
-        }
-
-        if (event.keyCode == o.focus &&
-                    event.metaKey == o.metafocus &&
-                    event.ctrlKey == o.ctrlfocus &&
-                    event.altKey == o.altfocus &&
-                    event.shiftKey == o.shiftfocus) {
-            focusPlayer();
-            return;
-        }
-
-        if (event.keyCode == o.user &&
-                    event.metaKey == o.metauser &&
-                    event.ctrlKey == o.ctrluser &&
-                    event.altKey == o.altuser &&
-                    event.shiftKey == o.shiftuser) {
-            goToUser();
-            return;
-        }
-
-        if (event.keyCode == o.like &&
-                    event.metaKey == o.metalike &&
-                    event.ctrlKey == o.ctrllike &&
-                    event.altKey == o.altlike &&
-                    event.shiftKey == o.shiftlike) {
-            pressLike();
-            return;
-        }
-
-        if (event.keyCode == o.dislike &&
-                    event.metaKey == o.metadislike &&
-                    event.ctrlKey == o.ctrldislike &&
-                    event.altKey == o.altdislike &&
-                    event.shiftKey == o.shiftdislike) {
-            pressDislike();
-            return;
-        }
-
-        if (event.keyCode == o.subscribe &&
-                    event.metaKey == o.metasubscribe &&
-                    event.ctrlKey == o.ctrlsubscribe &&
-                    event.altKey == o.altsubscribe &&
-                    event.shiftKey == o.shiftsubscribe ) {
-            pressSubscribe();
-            return;
-        }
-
-        if (event.keyCode == o.playlist &&
-                    event.metaKey == o.metaplaylist &&
-                    event.ctrlKey == o.ctrlplaylist &&
-                    event.altKey == o.altplaylist &&
-                    event.shiftKey == o.shiftplaylist) {
-            pressAddToPlaylist();
-            return;
-        }
-
-        if (event.keyCode == o.info &&
-                    event.metaKey == o.metainfo &&
-                    event.ctrlKey == o.ctrlinfo &&
-                    event.altKey == o.altinfo &&
-                    event.shiftKey == o.shiftinfo) {
-            pressMoreInfo();
-            return;
-        }
-
-        if (event.keyCode == o.listDown &&
-                    event.metaKey == o.metalistDown &&
-                    event.ctrlKey == o.ctrllistDown &&
-                    event.altKey == o.altlistDown &&
-                    event.shiftKey == o.shiftlistDown) {
-            pressListDown();
-            return;
-        }
-
-        if (event.keyCode == o.listUp &&
-                    event.metaKey == o.metalistUp &&
-                    event.ctrlKey == o.ctrllistUp &&
-                    event.altKey == o.altlistUp &&
-                    event.shiftKey == o.shiftlistUp) {
-            pressListUp();
-            return;
-        }
-
-        if (event.keyCode == o.tabLeft &&
-                    event.metaKey == o.metatabLeft &&
-                    event.ctrlKey == o.ctrltabLeft &&
-                    event.altKey == o.alttabLeft &&
-                    event.shiftKey == o.shifttabLeft) {
-            pressTabLeft();
-            return;
-        }
-
-        if (event.keyCode == o.tabRight &&
-                    event.metaKey == o.metatabRight &&
-                    event.ctrlKey == o.ctrltabRight &&
-                    event.altKey == o.alttabRight &&
-                    event.shiftKey == o.shifttabRight) {
-            pressTabRight();
-            return;
+        var i = 0;
+        for (i = 0; i < options.length; i++) {
+            if (event.keyCode == o[options[i]]&&
+                        event.metaKey == o["meta" + options[i]]&&
+                        event.ctrlKey == o["ctrl" + options[i]]&&
+                        event.altKey == o["meta" + options[i]]&&
+                        event.shiftKey == o["shift" + options[i]]) {
+                functions[options[i]]();
+                break;
+            }
         }
     });
 }
 
-function goToSubs() {
-    window.location.href = "https://www.youtube.com/feed/subscriptions";
-}
+var functions = {
+    subbox: function goToSubs() {
+        window.location.href = "https://www.youtube.com/feed/subscriptions";
+    },
 
-function goToUser() {
-    var link = document.querySelectorAll('.yt-user-photo')[0];
-    if (typeof link !== "undefined") {
-        // Go to user page from the video page.
-        window.location.href = link.href;
-    } else {
-        // Go to user from list entry
-        var userList = document.querySelectorAll('.yt-lockup-byline a');
-        window.location.href = userList[idx].href;
-    }
-}
+    user: function goToUser() {
+        var link = document.querySelectorAll('.yt-user-photo')[0];
+        if (typeof link !== "undefined") {
+            // Go to user page from the video page.
+            window.location.href = link.href;
+        } else {
+            // Go to user from list entry
+            var userList = document.querySelectorAll('.yt-lockup-byline a');
+            window.location.href = userList[idx].href;
+        }
+    },
 
-function focusPlayer() {
-    document.activeElement.blur();
-    var mp = document.getElementById("movie_player");
-    if (typeof mp !== "undefined") {
-        mp.focus();
-        window.scrollTo(0,0);
-    }
+    focus: function focusPlayer() {
+        document.activeElement.blur();
+        var mp = document.getElementById("movie_player");
+        if (typeof mp !== "undefined") {
+            mp.focus();
+            window.scrollTo(0,0);
+        }
+    },
+
+    // VIDEO PAGE SHORTCUTS
+    like: function pressLike() {
+        var button = $('.like-button-renderer-like-button');
+        click(button);
+    },
+    dislike: function pressDislike() {
+        var button = $('.like-button-renderer-dislike-button');
+        click(button);
+    },
+    click: function click(button) {
+        if (button[0].classList.contains('hid')) {
+            button[1].click();
+        } else {
+            button[0].click();
+        }
+    },
+    subscribe: function pressSubscribe() {
+        var buttons = $('.yt-uix-subscription-button');
+        if (buttons.length == 1) {
+            $('.yt-uix-subscription-button')[0].click();
+        }
+    },
+    playlist: function pressAddToPlaylist() {
+        $('.yt-uix-videoactionmenu-button')[0].click();
+    },
+    info: function pressMoreInfo() {
+        $('.yt-uix-button-expander')[0].click();
+    },
+
+    // List navigation shortcuts
+    listDown: function pressListDown() {
+        if (idx < $(selector_all).length-1) {
+            idx++;
+            select();
+            if (idx == $(selector_all).length-2) {
+                $(".yt-uix-load-more").click();
+            }
+        }
+    },
+    listUp: function pressListUp() {
+        if (idx > 0) {
+            idx--;
+            select();
+        }
+    },
+    tabLeft: function pressTabRight() {
+        if (localStorage.tidx < $(tab_selector_all).length - 1) {
+            var link = $(tab_selector.replace('*', localStorage.tidx));
+            location.href = link.attr('href');
+        }
+    },
+    tabRight: function pressTabLeft() {
+        if (localStorage.tidx > 0) {
+            localStorage.tidx--;
+            var link = $(tab_selector.replace('*', localStorage.tidx));
+            location.href = link.attr('href');
+        }
+    },
 }
 
 function unfocus() {
@@ -153,7 +121,7 @@ function unfocus() {
     b.blur();
 }
 
-// LIST NAVIGATION
+// LIST NAVIGATION HELPER
 var selector = "li div div div h3 a:nth(*)";
 var selector_all = selector.replace(':nth(*)', '');
 var previousSelection = null;
@@ -173,24 +141,7 @@ var select = function() {
     previousSelection = link.get()[0];
 };
 
-function pressListDown() {
-    if (idx < $(selector_all).length-1) {
-        idx++;
-        select();
-        if (idx == $(selector_all).length-2) {
-            $(".yt-uix-load-more").click();
-        }
-    }
-}
-
-function pressListUp() {
-    if (idx > 0) {
-        idx--;
-        select();
-    }
-}
-
-// NAV BAR SHORTCUTS
+// NAV BAR SHORTCUTS HELPER
 var tab_selector = ".yt-uix-button-epic-nav-item:nth(*)";
 var tab_selector_all = tab_selector.replace(':nth(*)', '');
 
@@ -206,53 +157,6 @@ function computeIndex() {
             i++;
         }
     });
-}
-
-function pressTabRight() {
-    if (localStorage.tidx < $(tab_selector_all).length - 1) {
-        var link = $(tab_selector.replace('*', localStorage.tidx));
-        location.href = link.attr('href');
-    }
-}
-
-function pressTabLeft() {
-    if (localStorage.tidx > 0) {
-        localStorage.tidx--;
-        var link = $(tab_selector.replace('*', localStorage.tidx));
-        location.href = link.attr('href');
-    }
-}
-
-// VIDEO PAGE SHORTCUTS
-function pressLike() {
-    var button = $('.like-button-renderer-like-button');
-    click(button);
-}
-function pressDislike() {
-    var button = $('.like-button-renderer-dislike-button');
-    click(button);
-}
-function click(button) {
-    if (button[0].classList.contains('hid')) {
-        button[1].click();
-    } else {
-        button[0].click();
-    }
-}
-
-function pressSubscribe() {
-    var buttons = $('.yt-uix-subscription-button');
-    if (buttons.length == 1) {
-        $('.yt-uix-subscription-button')[0].click();
-    }
-}
-
-function pressAddToPlaylist() {
-    $('.yt-uix-videoactionmenu-button')[0].click();
-}
-
-function pressMoreInfo() {
-    $('.yt-uix-button-expander')[0].click();
 }
 
 // LINK OPENING SHORTCUTS
@@ -280,7 +184,7 @@ key('shift+return', function(e) {
     }
 });
 
-// SHORTCUT MODALd
+// SHORTCUT MODAL
 // Inject the shortcut list.
 $.get(chrome.extension.getURL('/popup.html'), function(data) {
     $(data).appendTo('body');
@@ -292,9 +196,6 @@ key('shift+/', function(e) {
 });
 
 function correctListing() {
-    var options = ['focus', 'subbox', 'user', 'like', 'dislike', 'subscribe', 'playlist', 'info',
-                   'listDown', 'listUp', 'tabLeft', 'tabRight'];
-
     var key;
     for (var i = 0; i < options.length; i++) {
         key = setKey(options[i]);
@@ -311,10 +212,10 @@ function setKey(key) {
 
         if(ctrl || alt || meta || shift || code) {
             var result = '';
-            if(meta) result += 'Cmd + ';
-            if(ctrl) result += 'Ctrl + ';
-            if(alt) result += 'Alt + ';
-            if(shift) result += 'Shift + ';
+            if(meta) result += 'Cmd ';
+            if(ctrl) result += 'Ctrl ';
+            if(alt) result += 'Alt ';
+            if(shift) result += 'Shift ';
 
             result += translate(code);
 
@@ -327,7 +228,6 @@ function setKey(key) {
 
 function translate(code) {
     var result = '';
-
     if(code>=48 && code<=90) {
         result += String.fromCharCode(code).toLowerCase();
     } else if(code==37) {
@@ -375,6 +275,5 @@ function translate(code) {
     } else if(code !=91 && code !=93 && !(code >=16 && code <=19)) {
         result += '[' + code + ']';
     }
-
     return result;
 }
